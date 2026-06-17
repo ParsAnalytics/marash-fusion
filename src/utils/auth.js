6d33fd2c0be29002ad2bd94fec3ddf4b82665e1a
ParsAnalytics <ospaksoy@gmail.com>
2026-06-17 17:07:56 +0300

    chore: temporarily disable password protection

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,9 @@
 export const requireAuth = () => {
+  // Şifre koruması geçici olarak kaldırıldı (kullanıcı talebi). 
+  // Yeniden açmak için aşağıdaki kodların yorum satırlarını kaldırıp, üstteki return true; satırını silebilirsiniz.
+  return true;
+  
+  /*
   const password = window.prompt("Lütfen yetkili şifresini giriniz:");
   // Basit şifre: marash
   if (password === "marash") {
@@ -7,4 +12,5 @@ export const requireAuth = () => {
     alert("Hatalı şifre!");
   }
   return false;
+  */
 };
