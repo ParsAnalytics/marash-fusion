6dea600e1a5befb6306fb30fb4f1816f846f807c
ParsAnalytics <ospaksoy@gmail.com>
2026-06-17 17:09:24 +0300

    feat: re-enable password protection (accepts 'marash' or 'maraş')

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,16 +1,10 @@
 export const requireAuth = () => {
-  // Şifre koruması geçici olarak kaldırıldı (kullanıcı talebi). 
-  // Yeniden açmak için aşağıdaki kodların yorum satırlarını kaldırıp, üstteki return true; satırını silebilirsiniz.
-  return true;
-  
-  /*
   const password = window.prompt("Lütfen yetkili şifresini giriniz:");
   // Basit şifre: marash
-  if (password === "marash") {
+  if (password === "marash" || password === "maraş") {
     return true;
   } else if (password !== null) {
     alert("Hatalı şifre!");
   }
   return false;
-  */
 };
